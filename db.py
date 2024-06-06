da29d2762ec4fe31158648083cec28d07400ee62
AtsushiUtsumi <debtgx103@gmail.com>
2024-06-07 07:47:20 +0900

    SQLiteの初期テーブル作成処理追加

--- db.py
+++ db.py
@@ -5,4 +5,5 @@ conn = sqlite3.connect('sugumi.db')
 c = conn.cursor()
 for a in c.execute("select name from sqlite_master where type='table'"):
     print(a)
+c.execute("create table IF NOT EXISTS internationalization (id TEXT NOT NULL, ja TEXT NOT NULL, en TEXT NOT NULL, PRIMARY KEY (id))")
 conn.close()
