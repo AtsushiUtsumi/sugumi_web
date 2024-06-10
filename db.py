6d9aa8b521ec133496c05bac4ef542db6d89e7fd
AtsushiUtsumi <debtgx103@gmail.com>
2024-06-11 06:41:45 +0900

    初期テーブル作成スクリプト修正

--- db.py
+++ db.py
@@ -5,5 +5,12 @@ conn = sqlite3.connect('sugumi.db')
 c = conn.cursor()
 for a in c.execute("select name from sqlite_master where type='table'"):
     print(a)
-c.execute("create table IF NOT EXISTS internationalization (id TEXT NOT NULL, ja TEXT NOT NULL, en TEXT NOT NULL, PRIMARY KEY (id))")
+c.execute("CREATE TABLE IF NOT EXISTS internationalization (id TEXT NOT NULL, ja TEXT NOT NULL, en TEXT NOT NULL, PRIMARY KEY (id))")
+c.execute("CREATE TABLE IF NOT EXISTS presentation_info (url TEXT, screen_name TEXT, class_name TEXT, action TEXT)")
+c.execute("CREATE TABLE IF NOT EXISTS project_info (id INT, project_name TEXT, output_path TEXT, group_id TEXT, framework TEXT, language TEXT, PRIMARY KEY (id))")
+c.execute("CREATE TABLE IF NOT EXISTS table_info (table_id INT, table_name TEXT, project_id INT,  columns_info TEXT, PRIMARY KEY (table_id))")
+c.execute("INSERT INTO table_info VALUES(2, 'infrastructure2', 1, 'Integer:id INT(3), String:type_name VARCHAR(50), String:table_name VARCHAR(50)')")
+for a in c.execute("select * from table_info"):
+    print(a)
+conn.commit()
 conn.close()
